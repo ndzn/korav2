c943c7683dae977739fb14e8c04e6c266d4f9831
lucidwave <22899586+lucidwave@users.noreply.github.com>
2022-04-25 18:59:55 -0400

    initial commit

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+export default function() {
+    return (
+        <div className="container p-2 px-4 flex min-h-screen">
+          <main className='font-mono text-xs code text-white whitespace-pre'>
+            <h1>
+            [the void stares back]
+            {"\n"}
+            [say hello :)]
+            </h1>
+          </main>
+        </div>
+      );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import dedent from 'dedent';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+
+export const getServerSideProps = () => {
+  const quotes = ["Badass quote", "o.O", ":P", "oWo", "O.O", ":eyes:", "your dreams are no match for my memes"]
+  return {
+    props: {
+      quote: quotes[Math.floor(Math.random() * quotes.length)],
+    },
+  };
+}
+
+const Home: React.FC<{quote: string}> = ({quote}) => {
+  return (
+    <div className="container p-2 px-4 flex min-h-screen">
+      <main className='font-mono text-xs code text-white whitespace-pre'>
+        <h1 className="mb-10">
+          {dedent(`​
+                 .+%@-
+                -@@@.                 @@=   .#@#:
+               =@@@*                  @@+  +@%-   .=+*+=:   -=-:++  :=+*+- 
+               @@@@#                  @@+=@@=    *@%=:-%@#  #@@#=- #@#-:#@%
+              .@@@@@=                 @@@@@%:   -@@:   .@@+ #@%    .-++*%@@
+               #@@@@@#:         -*    @@* -@@*  -@@-   :@@= #@#   .@@*:.+@@
+               .%@@@@@@%*=---=*@%.    @@+  .#@@- +@@+=+@@*  #@#   :@@*=+%@@
+                 +@@@@@@@@@@@@@+      --.    ---.  :===-    :-:    .-==-.--
+                   -*%@@@@@%*-`)}
+        </h1>
+        <h1>
+          "{quote}"
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
